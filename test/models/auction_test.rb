c4a19bf82b795989db014847c57ededc4d371059
nenekelvin <karvinmitchar@gmail.com>
2019-11-18 16:45:53 +0300

    :bug: enter truths

--- test/models/auction_test.rb
+++ test/models/auction_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class AuctionTest < ActiveSupport::TestCase
- Rspec.describe Auction, :type => :model do |variable|
+ Rspec.describe Auction, :type => :model do 
+ it "is valid with valid attributes"
+  it "is not valid without a title"
+  it "is not valid without a description"
+  it "is not valid without a start_date"
+  it "is not valid without a end_date"
  	
  end
 
