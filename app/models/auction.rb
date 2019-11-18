768a8978a6d7eede5d9582080e483c6a09d0b988
nenekelvin <karvinmitchar@gmail.com>
2019-11-18 16:56:53 +0300

    association btwm models

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,3 +1,6 @@
-class Auction < ApplicationRecord
-	validates_presence_of :title
+class Auction < ActiveRecord::Base
+  has_one :buyer, class_name: "User"
+  has_one :seller, class_name: "User"
+
+  validates_presence_of :title, :description, :start_date, :end_date
 end
