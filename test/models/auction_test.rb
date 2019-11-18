c9ed487f7a33ab3cd23449d36201e914010513b6
nenekelvin <karvinmitchar@gmail.com>
2019-11-18 16:51:12 +0300

    test attributes

--- test/models/auction_test.rb
+++ test/models/auction_test.rb
@@ -5,6 +5,9 @@ RSpec.describe Auction, :type => :model do
 
   it "is valid with valid attributes" do
     subject.title = "Anything"
+    subject.description = "Anything"
+    subject.start_date = DateTime.now
+    subject.end_date = DateTime.now + 1.week
     expect(subject).to be_valid
   end
 
@@ -12,7 +15,21 @@ RSpec.describe Auction, :type => :model do
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a description"
-  it "is not valid without a start_date"
-  it "is not valid without a end_date"
+  it "is not valid without a description" do
+    subject.title = "Anything"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a start_date" do
+    subject.title = "Anything"
+    subject.description = "Lorem ipsum dolor sit amet"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a end_date" do
+    subject.title = "Anything"
+    subject.description = "Lorem ipsum dolor sit amet"
+    subject.start_date = DateTime.now
+    expect(subject).to_not be_valid
+  end
 end
